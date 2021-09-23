98caf49010c32341235f050684366d9e111319df
Jakub Kotulski <jakub.kotulski@interia.com>
2021-09-23 09:21:06 +0200

    clean up code

--- zadanie4.js
+++ zadanie4.js
@@ -1,7 +1,4 @@
-function objectToArray(objectVariable){
-    let result = Object.keys(objectVariable).map((key) => [String(key), objectVariable[key]]);
-    return result
-}
+const objectToArray = (objectVariable) => Object.keys(objectVariable).map((key) => [String(key), objectVariable[key]]);
 
 console.log(objectToArray({ D: 1, B: 2, C: 3 }));
 console.log(objectToArray({ likes: 2, dislikes: 3, followers: 10 }))
