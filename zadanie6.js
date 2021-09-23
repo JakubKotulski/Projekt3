fbe82a6398bd13460752e7d2fced76832f0eae5d
Jakub Kotulski <jakub.kotulski@interia.com>
2021-09-23 13:00:39 +0200

    second version

---
+++ zadanie6.js
@@ -0,0 +1,30 @@
+function danceForCash(input){
+    const pinNumber = [];
+    const danceMovesTable = ["Shimmy", "Shake", "Pirouette", "Slide", " ", " ", " ", "Pop", " ", "Arabesque"];
+    const output = [];
+    for(let i = 0; i < input.length; i++){
+        pinNumber.push(parseInt(input[i], 10));
+    }
+    for(let i = 0; i < pinNumber.length; i++){
+        let selectedMove = pinNumber[i] + i;
+        if(selectedMove > danceMovesTable.length - 1){
+            while(selectedMove > danceMovesTable.length - 1){
+                selectedMove = selectedMove - danceMovesTable.length;
+            }
+            output.push(danceMovesTable[selectedMove]);
+        }
+        else{
+            output.push(danceMovesTable[selectedMove]);
+        }
+        if(output[i] == " "){
+            return "Invalid value"
+        }
+    }
+    
+    return output
+}
+
+console.log(danceForCash("0000"));
+console.log(danceForCash("3856"));
+console.log(danceForCash("9999"));
+console.log(danceForCash("32a1"))
